43390ead3860e7d2d7e3c7ef2e07c4bab3ec9e20
TARISO <toni.ariso@gmail.com>
2020-08-31 06:36:01 +0200

    remove vtabs

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,26 +1,22 @@
 <template>
   <v-card class="mt-8">
-    <v-tabs v-model="timerType" grow>
-      <v-tab v-for="tab in tabTitles" :key="tab">{{tab}}</v-tab>
-      <v-tabs-items v-model="timerType">
-        <v-tab-item>
-          <v-card color="basil" class="pa-5 d-flex flex-column justify-center align-center" flat>
-            <h1 class="time">{{displayMinutes}}:{{displaySeconds}}</h1>
-            <div class="button-group">
-              <v-btn @click="start" color="primary">
-                <v-icon left small>mdi-play-circle-outline</v-icon>Start
-              </v-btn>
-              <v-btn @click="stop" color="error">
-                <v-icon left small>mdi-stop-circle-outline</v-icon>Stop
-              </v-btn>
-              <v-btn @click="reset">
-                <v-icon left small>mdi-restart</v-icon>Reset
-              </v-btn>
-            </div>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+    <v-tabs @change="changeCurrentTimer" v-model="currentTimer" grow>
+      <v-tab v-for="timer in timers" :key="timer.name">{{timer.name}}</v-tab>
     </v-tabs>
+    <v-card color="basil" class="pa-5 d-flex flex-column justify-center align-center" flat>
+      <h1 class="time">{{displayMinutes}}:{{displaySeconds}}</h1>
+      <div class="button-group">
+        <v-btn @click="start" color="primary">
+          <v-icon left small>mdi-play-circle-outline</v-icon>Start
+        </v-btn>
+        <v-btn @click="stop" color="error">
+          <v-icon left small>mdi-stop-circle-outline</v-icon>Stop
+        </v-btn>
+        <v-btn @click="reset" :disabled="isRunning">
+          <v-icon left small>mdi-restart</v-icon>Reset
+        </v-btn>
+      </div>
+    </v-card>
   </v-card>
 </template>
 
@@ -28,13 +24,15 @@
 export default {
   data() {
     return {
-      display: {
-        minutes: "00",
-        seconds: "00",
-      },
+      isRunning: false,
+      timerInstance: null,
       totalSeconds: 25 * 60,
-      timerType: 0,
-      tabTitles: ["Pomodoro", "Short Break", "Long Break"],
+      currentTimer: 0,
+      timers: [
+        { name: "Pomodoro", minutes: 25 },
+        { name: "Short Break", minutes: 5 },
+        { name: "Long Break", minutes: 10 },
+      ],
     };
   },
   computed: {
@@ -56,12 +54,23 @@ export default {
       return time.toString();
     },
     start() {
-      setInterval(() => {
+      this.stop();
+      this.isRunning = true;
+      this.timerInstance = setInterval(() => {
         this.totalSeconds -= 1;
       }, 1000);
     },
-    stop() {},
-    reset() {},
+    stop() {
+      this.isRunning = false;
+      clearInterval(this.timerInstance);
+    },
+    reset() {
+      this.stop();
+      this.totalSeconds = 25 * 60;
+    },
+    changeCurrentTimer(num) {
+      console.log(num);
+    },
   },
 };
 </script>
